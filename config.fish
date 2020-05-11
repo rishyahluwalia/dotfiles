44001df29a529a6454782e6fee33fab04250b02d
Rishy Ahluwalia <rishy.ahluwalia26@gmail.com>
2020-05-11 21:48:53 +1000

    add alacritty alias to fish config

--- config.fish
+++ config.fish
@@ -1,7 +1,12 @@
+set -x NVM_DIR ~/.nvm
 set -xU EDITOR nvim
-alias ll "ls -latrhs"
+
+nvm use default --silent
+
+alias ll "ls -lash"
 alias tmuxkill "tmux kill-server"
 alias tmuxmain "tmux new -s main"
 alias fishconfig "nvim ~/.config/fish/config.fish"
 alias nvimconfig "nvim ~/.config/nvim/init.vim"
 alias tmuxconfig "nvim ~/.tmux.conf"
+alias alacrittyconfig "nvim ~/.config/alacritty/alacritty.yml"
